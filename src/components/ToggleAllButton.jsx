0421760b28510c759a91bbbec2a91cecf0cfec5a
frontexas <forfron@gmail.com>
2017-10-27 15:29:20 -0500

    Move ToggleAll handler to ToggleAllButton

--- src/components/ToggleAllButton.jsx
+++ src/components/ToggleAllButton.jsx
@@ -1,5 +1,37 @@
-import TableActionButtonFactory from './TableActionButtonFactory';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'react-apollo';
 import { TOGGLE_ALL } from '../graph';
 
-const ToggleAllButton = TableActionButtonFactory(TOGGLE_ALL);
-export default ToggleAllButton;
+import TableActionButton from './TableActionButton';
+
+class ToggleAllButton extends Component {
+  static propTypes = {
+    toggleAllTodos: PropTypes.func.isRequired,
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      toggleAllValue: false,
+    };
+  }
+
+  handleOnClick = () => {
+    const { toggleAllTodos } = this.props;
+    toggleAllTodos(this.state.toggleAllValue)
+      .then(() => this.setState({ toggleAllValue: !this.state.toggleAllValue }));
+  }
+
+  render() {
+    return (
+      <TableActionButton onClick={this.handleOnClick} {...this.props} />
+    );
+  }
+}
+
+export default graphql(TOGGLE_ALL, {
+  props: ({ mutate }) => ({
+    toggleAllTodos: checked => mutate({ variables: { checked } }),
+  }),
+})(ToggleAllButton);
