eefc28afa2025d9eb60aa67df4c25ccc275a2cd9
frontexas <forfron@gmail.com>
2017-10-27 15:18:50 -0500

    Move ClearCompleted handler to ClearCompletedButton

--- src/components/ClearCompletedButton.jsx
+++ src/components/ClearCompletedButton.jsx
@@ -1,5 +1,36 @@
-import TableActionButtonFactory from './TableActionButtonFactory';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'react-apollo';
 import { CLEAR_COMPLETED } from '../graph';
 
-const ClearCompletedButton = TableActionButtonFactory(CLEAR_COMPLETED);
-export default ClearCompletedButton;
+import TableActionButton from './TableActionButton';
+
+class ClearCompletedButton extends Component {
+  static propTypes = {
+    clearCompleted: PropTypes.func.isRequired,
+    refetchTodos: PropTypes.func.isRequired,
+  }
+
+  handleOnClick = () => {
+    const { clearCompleted, refetchTodos } = this.props;
+    clearCompleted()
+      .then((res) => {
+        if (res.data.clearCompleted) {
+          refetchTodos();
+        }
+      });
+  }
+
+  render() {
+    return (
+      <TableActionButton onClick={this.handleOnClick} {...this.props} />
+    );
+  }
+}
+
+export default graphql(CLEAR_COMPLETED, {
+  props: ({ mutate }) => ({
+    clearCompleted: () => mutate(),
+  }),
+})(ClearCompletedButton);
+
