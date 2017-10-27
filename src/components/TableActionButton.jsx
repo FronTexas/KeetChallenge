b77851851138d28b1268e2de98ad0bc5e9f387e0
frontexas <forfron@gmail.com>
2017-10-27 15:18:01 -0500

    Make TableActionButton a presentational component

--- src/components/TableActionButton.jsx
+++ src/components/TableActionButton.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import { graphql } from 'react-apollo';
 import PropTypes from 'prop-types';
 import { Button } from 'react-md';
 
-
-const TableActionButton = ({ mobile, children, mutate, OnClickHandler, ...props }) => (
+const TableActionButton = ({ mobile, children, ...props }) => (
   <Button
     icon={mobile}
     flat={!mobile}
     primary
     tooltipLabel={mobile ? children : null}
-    onClick={() => OnClickHandler(mutate)}
     {...props}
   >
     {mobile ? null : children}
@@ -22,8 +19,6 @@ TableActionButton.defaultProps = { mobile: false };
 TableActionButton.propTypes = {
   mobile: PropTypes.bool,
   children: PropTypes.node.isRequired,
-  OnClickHandler: PropTypes.func.isRequired,
-  mutate: PropTypes.func.isRequired,
 };
 
-export default mutation => graphql(mutation)(TableActionButton);
+export default TableActionButton;
